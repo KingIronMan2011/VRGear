14a9af28c358bc67e418c221847f472772ec7ddb
Julian Speckmann <176780813+KingIronMan2011@users.noreply.github.com>
2026-07-31 03:53:43 +0200

    Add Quest items to assets and creative tabs
    Adds common item model and English localization assets for the Oculus headset/controller, and registers both items in the Tools & Utilities creative tab on Fabric and Forge. It also removes Fabric’s dependency on Forge-generated resources and excludes Forge’s en_us.json during resource processing to avoid duplicate/incorrect lang handling.

--- fabric/src/main/java/net/vrgear/fabric/VRGearModFabric.java
+++ fabric/src/main/java/net/vrgear/fabric/VRGearModFabric.java
@@ -1,11 +1,18 @@
 package net.vrgear.fabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.vrgear.VRGearMod;
+import net.vrgear.common.WCItems;
 
 public class VRGearModFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         VRGearMod.init();
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> {
+            entries.accept(WCItems.OCULUS_HEADSET);
+            entries.accept(WCItems.OCULUS_CONTROLLER);
+        });
     }
 }
--- forge/src/main/java/net/vrgear/forge/VRGearModForge.java
+++ forge/src/main/java/net/vrgear/forge/VRGearModForge.java
@@ -1,12 +1,24 @@
 package net.vrgear.forge;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.vrgear.VRGearMod;
+import net.vrgear.common.WCItems;
 
 @Mod(VRGearMod.MOD_ID)
+@Mod.EventBusSubscriber(modid = VRGearMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class VRGearModForge {
     public VRGearModForge() {
         VRGearMod.init();
     }
 
+    @SubscribeEvent
+    public static void addCreativeItems(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(WCItems.OCULUS_HEADSET);
+            event.accept(WCItems.OCULUS_CONTROLLER);
+        }
+    }
 }
